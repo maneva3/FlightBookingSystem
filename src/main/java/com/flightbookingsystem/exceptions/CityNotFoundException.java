54356eb8c9f895f11f61479e2fbe1cdd68b67eaf
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-19 02:01:46 +0300

    Add custom exceptions for NOT_FOUND HTTP status

---
+++ src/main/java/com/flightbookingsystem/exceptions/CityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.flightbookingsystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class CityNotFoundException extends RuntimeException {
+    public CityNotFoundException(String message) {
+        super(message);
+    }
+}
