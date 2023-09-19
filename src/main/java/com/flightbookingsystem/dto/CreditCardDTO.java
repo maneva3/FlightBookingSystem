65f67a4b7548b59d59acb787ae703ec0cff36479
Katerina Maneva <k.g.maneva@gmail.com>
2023-09-19 09:03:03 +0300

    Create DTO objects for part of the entities

---
+++ src/main/java/com/flightbookingsystem/dto/CreditCardDTO.java
@@ -0,0 +1,38 @@
+package com.flightbookingsystem.dto;
+
+import com.flightbookingsystem.data.enums.CreditCardType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class CreditCardDTO {
+    @Pattern(regexp = "[3-5][0-9]{7,18}", message = "Card number must be 8-16 digits")
+    private String cardNumber;
+
+    @NotBlank(message = "Card holder name is required")
+    private String cardHolderName;
+
+    @Future(message = "Expiry date must be in the future")
+    @NotNull(message = "Expiry date is required")
+    private LocalDate expiryDate;
+
+    @Pattern(regexp = "[0-9]{3}", message = "CVV must be 3 digits")
+    @NotBlank(message = "CVV is required")
+    private String cvv;
+
+    @Enumerated(EnumType.STRING)
+    private CreditCardType cardType;
+}
---
+++ src/main/java/com/flightbookingsystem/dto/IdentificationDTO.java
@@ -0,0 +1,45 @@
+package com.flightbookingsystem.dto;
+
+import com.flightbookingsystem.data.enums.CountryCode;
+import com.flightbookingsystem.data.enums.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class IdentificationDTO {
+
+    @Pattern(regexp = "[a-zA-Z0-9]{13}", message = "Identification number must be 13 symbols")
+    private String identificationNumber;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    @NotNull(message = "Expiry date is required")
+    @Future(message = "Expiry date must be in the future")
+    private LocalDate expiryDate;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Country name is required")
+    private CountryCode nationality;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Gender is required")
+    private Gender gender;
+}
