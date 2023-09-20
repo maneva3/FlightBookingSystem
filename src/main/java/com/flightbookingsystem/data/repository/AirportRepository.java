3f857b4335240bac84e6383ba2cf80aa8d740664
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-20 11:41:35 +0300

    Add repository classes (#10)

---
+++ src/main/java/com/flightbookingsystem/data/repository/AirportRepository.java
@@ -0,0 +1,16 @@
+package com.flightbookingsystem.data.repository;
+
+import com.flightbookingsystem.data.entity.Airport;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AirportRepository extends JpaRepository<Airport, String> {
+    List<Airport> findAllByName(String name);
+    List<Airport> findAllByCityName(String name);
+    List<Airport> findAllByCityNameStartingWith(String name, Sort sort);
+    List<Airport> findAllByNameStartingWith(String name, Sort sort);
+    List<Airport> findAllByCityNameAndNameStartingWith(String cityName, String name, Sort sort);
+    List<Airport> findAllByNameOrCityNameOrCodeOrCityCodeStartingWith(String cityName, String airportName, String airportCode, String cityCode, Sort sort);
+}
