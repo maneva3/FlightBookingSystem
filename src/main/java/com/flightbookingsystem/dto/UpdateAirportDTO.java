84ea1f5dd038baa62c786787dafaef5fa4c5d40b
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-28 02:09:45 +0300

    Add basic, create and update dtos. They are mostly empty

---
+++ src/main/java/com/flightbookingsystem/dto/UpdateAirportDTO.java
@@ -0,0 +1,24 @@
+package com.flightbookingsystem.dto;
+
+import com.flightbookingsystem.data.entity.City;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class UpdateAirportDTO {
+    @NotBlank(message = "Airport code cannot be blank!")
+    private String code;
+
+    @NotBlank(message = "Airport name cannot be blank!")
+    private String name;
+
+    @NotNull(message = "City must be set!")
+    private City city;
+}
