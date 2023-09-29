bc69209347bee5ee3b98c9e86fb42d498f42f6ab
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-09-29 03:55:38 +0300

    Create constructors and getters for all enums. This is needed so they can be visualized correctly in the UI. Add annotations to one DTO I missed in the last commit

--- src/main/java/com/flightbookingsystem/data/enums/CreditCardType.java
+++ src/main/java/com/flightbookingsystem/data/enums/CreditCardType.java
@@ -1,7 +1,16 @@
 package com.flightbookingsystem.data.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum CreditCardType {
-    VISA,
-    MASTERCARD,
-    AMERICAN_EXPRESS,
+    VISA("Visa"),
+    MASTERCARD("Mastercard"),
+    AMERICAN_EXPRESS("American Express");
+
+    private final String creditCardType;
+
+    CreditCardType(String creditCardType) {
+        this.creditCardType = creditCardType;
+    }
 }
