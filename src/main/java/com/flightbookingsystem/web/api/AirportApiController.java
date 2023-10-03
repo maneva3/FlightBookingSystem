c0d9edda27a39807347be0f082405bb8d92a4d57
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-04 02:53:42 +0300

    Add API controllers for Airports and City with just CRUD in it.

---
+++ src/main/java/com/flightbookingsystem/web/api/AirportApiController.java
@@ -0,0 +1,52 @@
+package com.flightbookingsystem.web.api;
+
+import com.flightbookingsystem.data.entity.Airport;
+import com.flightbookingsystem.dto.AirportDTO;
+import com.flightbookingsystem.dto.CreateAirportDTO;
+import com.flightbookingsystem.dto.UpdateAirportDTO;
+import com.flightbookingsystem.services.AirportService;
+import com.flightbookingsystem.web.view.model.AirportViewModel;
+import com.flightbookingsystem.web.view.model.CreateAirportViewModel;
+import com.flightbookingsystem.web.view.model.UpdateAirportViewModel;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/airports")
+public class AirportApiController {
+    private final AirportService airportService;
+    private final ModelMapper modelMapper;
+
+    private AirportViewModel convertToAirportViewModel(AirportDTO airportDTO) {
+        return modelMapper.map(airportDTO, AirportViewModel.class);
+    }
+
+    @GetMapping
+    public List<AirportDTO> getAirports() {
+        return airportService.getAirports();
+    }
+
+    @RequestMapping("/{code}")
+    public AirportDTO getAirport(@PathVariable("code") String code){
+        return airportService.getAirport(code);
+    }
+
+    @PostMapping
+    public Airport createAirport(@RequestBody CreateAirportViewModel airport) {
+        return airportService.create(modelMapper.map(airport, CreateAirportDTO.class));
+    }
+
+    @PutMapping("/{code}")
+    public Airport updateAirport(@PathVariable("code") String code, @RequestBody UpdateAirportViewModel airport) {
+        return airportService.updateAirport(code, modelMapper.map(airport, UpdateAirportDTO.class));
+    }
+
+    @DeleteMapping("/{code}")
+    public void deleteAirport(@PathVariable("code") String code) {
+        airportService.deleteAirport(code);
+    }
+}
---
+++ src/main/java/com/flightbookingsystem/web/api/CityApiController.java
@@ -0,0 +1,51 @@
+package com.flightbookingsystem.web.api;
+
+import com.flightbookingsystem.data.entity.City;
+import com.flightbookingsystem.data.entity.Country;
+import com.flightbookingsystem.dto.*;
+import com.flightbookingsystem.services.CityService;
+import com.flightbookingsystem.services.CountryService;
+import com.flightbookingsystem.web.view.model.*;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/cities")
+public class CityApiController {
+    private final CityService cityService;
+    private final ModelMapper modelMapper;
+
+    private CityViewModel convertToCityViewModel(CityDTO cityDTO) {
+        return modelMapper.map(cityDTO, CityViewModel.class);
+    }
+
+    @GetMapping
+    public List<CityDTO> getCities() {
+        return cityService.getCities();
+    }
+
+    @RequestMapping("/{code}")
+    public CityDTO getCity(@PathVariable("code") String code){
+        return cityService.getCity(code);
+    }
+
+    @PostMapping
+    public City createCity(@RequestBody CreateCityViewModel city) {
+        return cityService.create(modelMapper.map(city, CreateCityDTO.class));
+    }
+
+    @PutMapping("/{code}")
+    public City updateCity(@PathVariable("code") String code, @RequestBody UpdateCityViewModel city) {
+        return cityService.updateCity(code, modelMapper.map(city, UpdateCityDTO.class));
+    }
+
+    @DeleteMapping("/{code}")
+    public void deleteCity(@PathVariable("code") String code) {
+        cityService.deleteCity(code);
+    }
+
+
+}
