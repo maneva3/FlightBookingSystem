e8e670457394a0c387ae85f7170cd263d7c0ec7d
Katerina Maneva <119162851+maneva3@users.noreply.github.com>
2023-10-05 02:59:21 +0300

    Add api controllers with main requests (#25)

---
+++ src/main/java/com/flightbookingsystem/web/api/FlightApiController.java
@@ -0,0 +1,48 @@
+package com.flightbookingsystem.web.api;
+
+import com.flightbookingsystem.data.entity.Flight;
+import com.flightbookingsystem.dto.*;
+import com.flightbookingsystem.services.FlightService;
+import com.flightbookingsystem.web.view.model.*;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/flights")
+public class FlightApiController {
+    private final FlightService flightService;
+    private final ModelMapper modelMapper;
+
+    private FlightViewModel convertToFlightViewModel(FlightDTO flightDTO) {
+        return modelMapper.map(flightDTO, FlightViewModel.class);
+    }
+
+    @GetMapping
+    public List<FlightDTO> getFlights() {
+        return flightService.getFlights();
+    }
+
+    @RequestMapping("/{id}")
+    public FlightDTO getFlight(@PathVariable("id") Long id){
+        return flightService.getFlight(id);
+    }
+
+    @PostMapping
+    public Flight createFlight(@RequestBody CreateFlightViewModel flight) {
+        return flightService.create(modelMapper.map(flight, CreateFlightDTO.class));
+    }
+
+    @PutMapping("/{id}")
+    public Flight updateFlight(@PathVariable("id") Long id, @RequestBody UpdateFlightViewModel flight) {
+        return flightService.updateFlight(id, modelMapper.map(flight, UpdateFlightDTO.class));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAFlight(@PathVariable("id") Long id) {
+        flightService.deleteFlight(id);
+    }
+}
