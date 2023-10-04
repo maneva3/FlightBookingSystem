463e5aa23fee74d3b6ec35b49d76417b46c4631e
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-04 19:39:52 +0300

    Add queries to Airport, City and Country repositories. Each of them gets all Airports/Cities/Countries by their name or code that contains certain string.

--- src/main/java/com/flightbookingsystem/data/repository/CountryRepository.java
+++ src/main/java/com/flightbookingsystem/data/repository/CountryRepository.java
@@ -1,8 +1,10 @@
 package com.flightbookingsystem.data.repository;
 
+import com.flightbookingsystem.data.entity.Airport;
 import com.flightbookingsystem.data.entity.City;
 import com.flightbookingsystem.data.entity.Country;
 import com.flightbookingsystem.data.enums.CountryCode;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +14,5 @@ public interface CountryRepository extends JpaRepository<Country, Long> {
     List<Country> findAllByCode(CountryCode countryCode);
     Country findCountryByCitiesContains(City city);
     List<Country> findAllByCitiesContains(City city);
+    List<Country> findAllByNameContainingIgnoreCaseOrCodeContainingIgnoreCaseOrderByNameAsc(String name, String code);
 }
