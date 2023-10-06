0e29da0a1c84ced386f3790d306f3db5f2c48f07
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-06 16:17:29 +0300

    Create CRUD for missing repositories

--- src/main/java/com/flightbookingsystem/data/repository/IdentificationDocumentRepository.java
+++ src/main/java/com/flightbookingsystem/data/repository/IdentificationDocumentRepository.java
@@ -4,5 +4,9 @@ import com.flightbookingsystem.data.entity.IdentificationDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IdentificationDocumentRepository extends JpaRepository<IdentificationDocument, String> {
-
+    IdentificationDocument findByIdentificationNumber(String documentNumber);
+    IdentificationDocument findByFirstNameAndLastName(String firstName, String lastName);
+    IdentificationDocument findByPersonalInfoFirstName(String firstName);
+    IdentificationDocument findByPersonalInfoPhoneNumber(String phoneNumber);
+    IdentificationDocument findByPersonalInfoUserUsername(String username);
 }
