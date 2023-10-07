296ca1244a75bc29c9eeb7c06b7503bc0a9914be
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-08 02:49:04 +0300

    Add User, Login and register API controllers. They don't work yet

---
+++ src/main/java/com/flightbookingsystem/web/api/LoginApiController.java
@@ -0,0 +1,35 @@
+package com.flightbookingsystem.web.api;
+
+import com.flightbookingsystem.data.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/login")
+public class LoginApiController {
+    @GetMapping("/login")
+    public String getLogin(Model model, Authentication authentication) {
+        Authentication authentication2 = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("username", authentication.getName());
+
+        User principal = (User) authentication.getPrincipal();
+        model.addAttribute("username", principal.getAuthorities());
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "login";
+    }
+
+    @GetMapping("/unauthorized")
+    public String unauthorized(){
+        return "unauthorized";
+    }
+}
