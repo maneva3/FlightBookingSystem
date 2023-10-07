28e4ee967282d5a1c965994b00ae9898e2929193
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-08 02:52:46 +0300

    Add user view models

---
+++ src/main/java/com/flightbookingsystem/web/view/model/UserViewModel.java
@@ -0,0 +1,17 @@
+package com.flightbookingsystem.web.view.model;
+
+import com.flightbookingsystem.data.entity.City;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class UserViewModel {
+
+}
