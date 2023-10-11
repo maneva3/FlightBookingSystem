5d8792962b76cde24a23207f48ef98d13a53a880
Georgi Gazepov <georgi.gazepov@gmail.com>
2023-10-11 04:31:05 +0300

    Change the return value due to spring security. The mighty internet suggested me to do that

--- src/main/java/com/flightbookingsystem/data/repository/UserRepository.java
+++ src/main/java/com/flightbookingsystem/data/repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     List<User> findAllByPersonalInfoFirstName(String firstName);
     List<User> findAllByPersonalInfoLastName(String lastName);
     User findByPersonalInfoPhoneNumber(String phoneNumber);
